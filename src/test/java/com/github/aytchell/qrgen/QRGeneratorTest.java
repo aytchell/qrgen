85d6dbe9169b107cfb741d81fd207b0b01e1020a
Hannes Lerchl <hannes.lerchl@googlemail.com>
2022-05-10 23:58:33 +0200

    Started writing unit tests

---
+++ src/test/java/com/github/aytchell/qrgen/QRGeneratorTest.java
@@ -0,0 +1,99 @@
+package com.github.aytchell.qrgen;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SuppressWarnings("ResultOfMethodCallIgnored")
+public class QRGeneratorTest {
+    private static Stream<Integer> imageSizesForSizeTest() {
+        final List<Integer> sizes = new ArrayList<>();
+        sizes.add(100);
+        sizes.add(300);
+        sizes.add(650);
+        sizes.add(900);
+        return sizes.stream();
+    }
+
+    @Test
+    void cloneIsPossible() {
+        // we only check that a clone is produced (not Exception, no null return value)
+        final QRGenerator gen = new QRGenerator();
+        final QRGenerator clone = gen.clone();
+        assertNotNull(clone);
+    }
+
+    @ParameterizedTest
+    @MethodSource("imageSizesForSizeTest")
+    void sizeIsRespected(Integer size) throws IOException, QRGenerationException {
+        final QRGenerator gen = new QRGenerator()
+                .withSize(size, size);
+        final Path path = gen.generate("Hello, World!");
+        try {
+            TestUtilities.assertFileExistsAndNotEmpty(path);
+            final BufferedImage img = TestUtilities.readProducedFile(path);
+            assertEquals(size, img.getWidth());
+            assertEquals(size, img.getHeight());
+        } finally {
+            path.toFile().delete();
+        }
+    }
+
+    @Test
+    void generatorCanCreatePngFiles() throws IOException, QRGenerationException {
+        final QRGenerator gen = new QRGenerator().as(ImageType.PNG);
+        final Path path = gen.generate("Hello PNG file");
+        try {
+            final ImageType type = TestUtilities.findOutImageTypeOfFile(path);
+            assertEquals(ImageType.PNG, type);
+        } finally {
+            path.toFile().delete();
+        }
+    }
+
+    @Test
+    void generatorCanCreateBmpFiles() throws IOException, QRGenerationException {
+        final QRGenerator gen = new QRGenerator().as(ImageType.BMP);
+        final Path path = gen.generate("Hello BMP file");
+        try {
+            final ImageType type = TestUtilities.findOutImageTypeOfFile(path);
+            assertEquals(ImageType.BMP, type);
+        } finally {
+            path.toFile().delete();
+        }
+    }
+
+    @Test
+    void generatorCanCreateGifFiles() throws IOException, QRGenerationException {
+        final QRGenerator gen = new QRGenerator().as(ImageType.GIF);
+        final Path path = gen.generate("Hello GIF file");
+        try {
+            final ImageType type = TestUtilities.findOutImageTypeOfFile(path);
+            assertEquals(ImageType.GIF, type);
+        } finally {
+            path.toFile().delete();
+        }
+    }
+
+    @Test
+    void generatorCanCreateJpgFiles() throws IOException, QRGenerationException {
+        final QRGenerator gen = new QRGenerator().as(ImageType.JPG);
+        final Path path = gen.generate("Hello JPG file");
+        try {
+            final ImageType type = TestUtilities.findOutImageTypeOfFile(path);
+            assertEquals(ImageType.JPG, type);
+        } finally {
+            path.toFile().delete();
+        }
+    }
+}
---
+++ src/test/java/com/github/aytchell/qrgen/TestUtilities.java
@@ -0,0 +1,100 @@
+package com.github.aytchell.qrgen;
+
+import org.junit.jupiter.api.Test;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestUtilities {
+    private static final byte bx89 = toByte(0x89);
+    private static final byte bxff = toByte(0xff);
+    private static final byte bxd8 = toByte(0xd8);
+    private static final byte bxe0 = toByte(0xe0);
+
+    private static final byte[] MAGIC_BYTES_BMP = { 0x42, 0x4d };               // ascii 'BM'
+    private static final byte[] MAGIC_BYTES_GIF = { 0x47, 0x49, 0x46, 0x38 };   // ascii 'GIF8'
+    private static final byte[] MAGIC_BYTES_PNG = { bx89, 0x50, 0x4e, 0x47 };   // ascii '.PNG'
+    private static final byte[] MAGIC_BYTES_JPG = { bxff, bxd8, bxff, bxe0 };   // ascii '....'
+
+    private static byte toByte(int value) {
+        if (value <= 127) return (byte)value;
+        return (byte)(value - 256);
+        // 128(int) = 0x80 = -128(byte) = 128 - 256
+        // 129(int) = 0x81 = -127(byte) = 129 - 256
+        // 130(int) = 0x82 = -126(byte) = 130 - 256
+        // 131(int) = 0x83 = -125(byte) = 131 - 256
+        // 132(int) = 0x84 = -124(byte) = 132 - 256
+        // 133(int) = 0x85 = -123(byte) = 133 - 256
+        // 134(int) = 0x86 = -122(byte) = 134 - 256
+        // 135(int) = 0x87 = -121(byte) = 135 - 256
+    }
+
+    @Test
+    void byteConversionsForMagicNumbersAreCorrect() {
+        assertEquals(0x89, ((int)toByte(0x89) & 0xff));
+        assertEquals(0xFF, ((int)toByte(0xFF) & 0xff));
+        assertEquals(0xd8, ((int)toByte(0xd8) & 0xff));
+        assertEquals(0xe0, ((int)toByte(0xe0) & 0xff));
+    }
+
+
+    public static BufferedImage readProducedFile(Path path) throws IOException {
+        final BufferedImage img = ImageIO.read(path.toFile());
+        assertNotNull(img);
+        return img;
+    }
+
+    public static ImageType findOutImageTypeOfFile(Path path) throws IOException {
+        try (final InputStream input = Files.newInputStream(path)) {
+            final int NUMBER_MAGIC_BYTES = MAGIC_BYTES_GIF.length;
+
+            // read the first bytes of the file and compare with well-known magic byte sequences
+            final byte[] magic = new byte[NUMBER_MAGIC_BYTES];
+            final int bytesRead = input.read(magic);
+            assertEquals(NUMBER_MAGIC_BYTES, bytesRead);
+
+            // the following checks only work properly if the arrays have the same size
+            assertEquals(NUMBER_MAGIC_BYTES, MAGIC_BYTES_GIF.length);
+            assertEquals(NUMBER_MAGIC_BYTES, MAGIC_BYTES_PNG.length);
+            assertEquals(NUMBER_MAGIC_BYTES, MAGIC_BYTES_JPG.length);
+
+            if (Arrays.equals(magic, MAGIC_BYTES_PNG)) {
+                return ImageType.PNG;
+            }
+            if (Arrays.equals(magic, MAGIC_BYTES_JPG)) {
+                return ImageType.JPG;
+            }
+            if (Arrays.equals(magic, MAGIC_BYTES_GIF)) {
+                return ImageType.GIF;
+            }
+
+            // the magic byte sequence of BMP is shorter
+            assertTrue(NUMBER_MAGIC_BYTES > MAGIC_BYTES_BMP.length);
+            final byte[] bmpMagic = new byte[MAGIC_BYTES_BMP.length];
+            System.arraycopy(magic, 0, bmpMagic, 0, MAGIC_BYTES_BMP.length);
+
+            if (Arrays.equals(bmpMagic, MAGIC_BYTES_BMP)) {
+                return ImageType.BMP;
+            }
+
+            return null;
+        }
+    }
+
+    public static void assertFileExistsAndNotEmpty(Path file200) throws IOException {
+        assertNotNull(file200);
+        assertTrue(file200.toFile().exists());
+        final BasicFileAttributes attrs = Files.readAttributes(file200, BasicFileAttributes.class);
+        assertTrue(attrs.isRegularFile());
+        assertTrue(attrs.size() > 0);
+    }
+}
