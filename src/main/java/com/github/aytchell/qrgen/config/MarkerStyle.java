55b88e8f18e2ed26e3b6184ba1c4af301a0d47b2
Hannes Lerchl <hannes.lerchl@googlemail.com>
2023-06-06 09:11:30 +0200

    Added new MarkerRenderer with raindrops 'falling' outwards

--- src/main/java/com/github/aytchell/qrgen/config/MarkerStyle.java
+++ src/main/java/com/github/aytchell/qrgen/config/MarkerStyle.java
@@ -3,5 +3,6 @@ package com.github.aytchell.qrgen.config;
 public enum MarkerStyle {
     RECTANGLES,
     ROUND_CORNERS,
-    CIRCLES
+    CIRCLES,
+    DROP_OUT
 }
--- src/main/java/com/github/aytchell/qrgen/renderers/marker/MarkerRenderer.java
+++ src/main/java/com/github/aytchell/qrgen/renderers/marker/MarkerRenderer.java
@@ -3,6 +3,7 @@ package com.github.aytchell.qrgen.renderers.marker;
 import com.github.aytchell.qrgen.renderers.ImgParameters;
 
 import java.awt.*;
+import java.awt.geom.AffineTransform;
 
 public abstract class MarkerRenderer {
     // regardless of the size of the payload or the error correction level
@@ -31,5 +32,21 @@ public abstract class MarkerRenderer {
 
     protected void renderBottomLeftMarker(Graphics2D gfx, int cellSize, ImgParameters imgParams) {
         renderTopLeftMarker(gfx, cellSize, imgParams);
+    }
+
+    protected void renderRotated(
+            double angle, Graphics2D gfx, int cellSize, ImgParameters imgParams, SingleMarkerRenderer renderer) {
+        final AffineTransform transform = gfx.getTransform();
+        final int halfMarkerSize = (cellSize * SIZE_OF_POSITION_MARKER) / 2;
+        gfx.translate(halfMarkerSize, halfMarkerSize);
+        gfx.rotate(angle);
+        gfx.translate(-halfMarkerSize, -halfMarkerSize);
+        renderer.renderMarker(gfx, cellSize, imgParams);
+        gfx.setTransform(transform);
+    }
+
+    @FunctionalInterface
+    protected interface SingleMarkerRenderer {
+        void renderMarker(Graphics2D gfx, int cellSize, ImgParameters imgParams);
     }
 }
